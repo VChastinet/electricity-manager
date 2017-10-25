bc69e77f3544e7cfe5da84b2c15ba8c524d0c0fe
Vchastinet <victorchastinet@live.com>
2017-10-25 12:29:35 -0300

    implemented "dexie" to use indexed db

--- src/app/device-data.service.ts
+++ src/app/device-data.service.ts
@@ -1,24 +1,44 @@
 import { Injectable } from '@angular/core';
 import { DeviceComponent } from './device.component'
 
+import { DexieService } from './core/dexie.service';
+import  Dexie from 'dexie';
+
+export interface Device {
+  deviceInput: DeviceComponent;
+  done: boolean;
+}
+
+export interface DeviceWithID extends Device {
+  id: number;
+}
+
 
 @Injectable()
 
 export class DeviceDataService {
 
-  public devices: DeviceComponent;
+  table: Dexie.Table<DeviceWithID, number>;
 
-  constructor() {
-    
+  constructor(private dexieService: DexieService) {
+    this.table = this.dexieService.table('devices');
    }
 
 
-  add (device: DeviceComponent){
-    this.devices = device;
+   getAll() {
+    return this.table.toArray();
   }
 
-  getDevices(){
-    return this.devices
+  add(data) {
+    return this.table.add(data);
+  }
+
+  update(id, data) {
+    return this.table.update(id, data);
+  }
+
+  remove(id) {
+    return this.table.delete(id);
   }
 }
 
